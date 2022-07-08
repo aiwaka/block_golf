4dbfa253fdaed3796bc8709020789b221e826421
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-07-09 04:19:16 +0900

    remove unnecessary attr

--- src/systems/ball.rs
+++ src/systems/ball.rs
@@ -25,15 +25,14 @@ fn spawn_ball(mut commands: Commands, key_in: Res<Input<KeyCode>>) {
     }
 }
 
-fn move_ball(commands: Commands, mut ball_query: Query<(&mut Ball, &mut Transform)>) {
-    for (mut ball, mut transform) in ball_query.iter_mut() {
+fn move_ball(commands: Commands, mut ball_query: Query<(&Ball, &mut Transform)>) {
+    for (ball, mut transform) in ball_query.iter_mut() {
         let current_pos = transform.translation;
         let new_pos = Vec2::new(
             ball.direction.x + current_pos.x,
             ball.direction.y + current_pos.y,
         );
         transform.translation = Vec3::new(new_pos.x, new_pos.y, 11.0);
-        ball.pos = new_pos;
     }
 }
 
