4e371cd10ce9f7b38d4d279d107dc4d0990b4034
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-07-10 15:13:51 +0900

    remove physics engine

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ mod systems;
 
 use bevy::{input::keyboard::keyboard_input_system, prelude::*};
 use bevy_prototype_lyon::prelude::*;
-// use heron::prelude::*;
 
 use systems::{
     ball::BallPlugin, block::BlockPlugin, collision::CollisionPlugin, field::FieldPlugin,
@@ -25,7 +24,6 @@ fn main() {
     app.add_plugins(DefaultPlugins);
     app.add_system(keyboard_input_system);
     app.add_plugin(ShapePlugin);
-    // app.add_plugin(PhysicsPlugin::default());
     app.add_system(bevy::input::system::exit_on_esc_system);
     app.add_startup_system(global_setup);
     app.add_plugin(FieldPlugin);
@@ -35,44 +33,3 @@ fn main() {
     app.add_plugin(CollisionPlugin);
     app.run();
 }
-
-// fn spawn(mut commands: Commands) {
-//     let shape = shapes::RegularPolygon {
-//         sides: 6,
-//         feature: shapes::RegularPolygonFeature::Radius(50.0),
-//         ..shapes::RegularPolygon::default()
-//     };
-//     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-//     commands
-//         .spawn_bundle(GeometryBuilder::build_as(
-//             &shape,
-//             DrawMode::Outlined {
-//                 fill_mode: FillMode::color(Color::CYAN),
-//                 outline_mode: StrokeMode::new(Color::BLACK, 7.0),
-//             },
-//             Transform::default(),
-//         ))
-//         .insert(RigidBody::Dynamic)
-//         .insert(CollisionShape::Sphere { radius: 50.0 })
-//         .insert(Velocity::from_linear(Vec3::X * 2.0 + Vec3::Y * 4.0))
-//         .insert(Acceleration::from_linear(Vec3::X * 1.0))
-//         .insert(PhysicMaterial {
-//             friction: 1.0,
-//             density: 10.0,
-//             ..Default::default()
-//         })
-//         .insert(RotationConstraints::lock())
-//         .insert(
-//             CollisionLayers::none()
-//                 .with_group(Layer::Player)
-//                 .with_mask(Layer::World),
-//         );
-// }
-
-// Define your physics layers
-// #[derive(PhysicsLayer)]
-// enum Layer {
-//     World,
-//     Player,
-//     Enemies,
-// }
