c7c1d516f2621a24f9c477313f9a7b1dc7a913bc
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-07-21 13:43:14 +0900

    fixed process for retrieving balls

--- src/systems/info_board.rs
+++ src/systems/info_board.rs
@@ -1,24 +1,24 @@
 //! プレイヤーに見えるゲーム情報を表示するシステム等
 use bevy::prelude::*;
-use bevy_prototype_lyon::prelude::*;
 
 use crate::components::{
-    ball::{SetBallEvent, SpawnBallEvent},
-    info::{ConsumingBall, MagazineUpdating, RemainingBall},
+    ball::SpawnBallEvent,
+    info::{ConsumingBall, RemainingBall},
     launcher::BallMagazine,
 };
 
-/// ボール出現時に箱に更新中マーカーを付与する
-fn insert_updateing_marker(
+/// ボール出現時に箱の先頭のボールに更新中マーカーを付与し, 箱から取り出す
+fn pop_ball_from_magazine(
     mut commands: Commands,
-    magazine_query: Query<(&BallMagazine, Entity)>,
+    mut magazine_query: Query<&mut BallMagazine>,
     mut spawn_ball_event_reader: EventReader<SpawnBallEvent>,
 ) {
     for _ in spawn_ball_event_reader.iter() {
-        let (mag, ent) = magazine_query.single();
-        commands.entity(ent).insert(MagazineUpdating);
+        let mut mag = magazine_query.single_mut();
+        // TODO: VecDequeを使うと計算量を減らせる
+        let (_, top_ball_ent) = mag.balls.remove(0);
         // 1つ目に消費中マーカーをつける
-        commands.entity(mag.balls[0].1).insert(ConsumingBall);
+        commands.entity(top_ball_ent).insert(ConsumingBall);
     }
 }
 
@@ -28,29 +28,20 @@ fn update_remaining_balls_info(
         (&mut Transform, Option<&ConsumingBall>, Entity),
         With<RemainingBall>,
     >,
-    mut magazine_query: Query<(&mut BallMagazine, Entity), With<MagazineUpdating>>,
 ) {
-    if magazine_query.iter().next().is_none() {
-        return;
-    }
     if let Some((mut trans, _, ent)) = remaining_ball_query.iter_mut().find(|q| q.1.is_some()) {
         // consumingballが存在していたらその処理だけ行う
         trans.scale *= 0.8;
         if trans.scale.x < 0.01 {
             commands.entity(ent).despawn();
         }
-    } else {
-        let (mut mag, ent) = magazine_query.single_mut();
-        // TODO: VecDequeを使うと計算量を減らせる
-        mag.balls.remove(0);
-        commands.entity(ent).remove::<MagazineUpdating>();
     }
 }
 
 pub struct InfoBoardPlugin;
 impl Plugin for InfoBoardPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(insert_updateing_marker);
+        app.add_system(pop_ball_from_magazine);
         app.add_system(update_remaining_balls_info);
     }
 }
