68cfcfbab291d65612f664d3e2ea12af346e73e3
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-08-12 13:34:54 +0900

    add goal to tutorial

--- src/stages/tutorial_stage.rs
+++ src/stages/tutorial_stage.rs
@@ -2,19 +2,12 @@ use std::f32::consts::FRAC_PI_2;
 
 use bevy::prelude::*;
 
-use super::structs::{
-    ArrangeBallInfo, BlockInfo, BlockShapeInfo, GoalInfo, LauncherInfo, StageInfo,
-};
+use super::structs::{ArrangeBallInfo, BlockInfo, GoalInfo, LauncherInfo, StageInfo};
 use super::{field_blocks::field_block, structs::BallInfo};
-use crate::components::physics::material::PhysicMaterial;
-use crate::components::{
-    ball::BallType,
-    block::{RotateStrategy, SlideStrategy},
-};
+use crate::components::ball::BallType;
 use crate::systems::field::{FIELD_HEIGHT, FIELD_WIDTH};
 
 pub fn tutorial_stage1() -> StageInfo {
-    let material = PhysicMaterial::new(1.0, 1.0, 0.0);
     let block_list = vec![];
 
     let launcher_info = LauncherInfo {
@@ -28,11 +21,18 @@ pub fn tutorial_stage1() -> StageInfo {
     let mut ball_list = Vec::<BallInfo>::new();
     ball_list.set_balls(BallType::Normal, 5);
 
-    let goal_list = vec![GoalInfo {
-        pos: Vec2::new(FIELD_WIDTH / 2.0 - 30.0, -FIELD_HEIGHT / 2.0 + 30.0),
-        radius: 40.0,
-        score: 1,
-    }];
+    let goal_list = vec![
+        GoalInfo {
+            pos: Vec2::new(FIELD_WIDTH / 2.0 - 30.0, -FIELD_HEIGHT / 2.0 + 30.0),
+            radius: 40.0,
+            score: 1,
+        },
+        GoalInfo {
+            pos: Vec2::new(-FIELD_WIDTH / 2.0 + 20.0, FIELD_HEIGHT / 2.0 - 20.0),
+            radius: 30.0,
+            score: 2,
+        },
+    ];
 
     StageInfo {
         stage_title: "tutorial1",
