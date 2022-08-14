500967fd7adfc1b68f8302c6f659a20eec330e17
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-08-14 14:11:42 +0900

    removed the unused and also confusing attributes

--- src/components/ball.rs
+++ src/components/ball.rs
@@ -56,26 +56,18 @@ pub struct BallNocking;
 
 #[derive(Component)]
 pub struct Ball {
-    pub pos: Vec2,
-    pub direction: Vec2, // 絶対値をspeedとする
     pub ball_type: BallType,
 }
 impl Default for Ball {
     fn default() -> Self {
         Ball {
-            pos: Vec2::ZERO,
-            direction: Vec2::new(4.0, 0.0),
             ball_type: BallType::Normal,
         }
     }
 }
 impl Ball {
-    pub fn new(pos: Vec2, direction: Vec2, ball_type: BallType) -> Self {
-        Self {
-            pos,
-            direction,
-            ball_type,
-        }
+    pub fn new(ball_type: BallType) -> Self {
+        Self { ball_type }
     }
 }
 
