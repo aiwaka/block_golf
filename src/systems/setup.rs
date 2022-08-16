77336f3555825127623ae170e1d27e7b27f5cc23
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-08-16 20:39:56 +0900

    camera bundle

--- src/systems/setup.rs
+++ src/systems/setup.rs
@@ -2,6 +2,5 @@ use bevy::prelude::*;
 
 pub fn global_setup(mut commands: Commands) {
     // カメラのセット
-    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(UiCameraBundle::default());
+    commands.spawn_bundle(Camera2dBundle::default());
 }
