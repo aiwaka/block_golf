314bf2115ec507d49d652cd3ce83ca2fe11d0e41
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-08-17 22:41:57 +0900

    implement gravity system

--- src/systems/physics/mod.rs
+++ src/systems/physics/mod.rs
@@ -1,11 +1,13 @@
+pub mod gravity;
 pub mod motion_dynamics;
 
-use self::motion_dynamics::MotionDynamicsPlugin;
+use self::{gravity::GravityPlugin, motion_dynamics::MotionDynamicsPlugin};
 use bevy::prelude::Plugin;
 
 pub struct PhysicsPlugin;
 impl Plugin for PhysicsPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.add_plugin(MotionDynamicsPlugin);
+        app.add_plugin(GravityPlugin);
     }
 }
