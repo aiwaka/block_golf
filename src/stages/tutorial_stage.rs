f3163a6b4184922c11950335db5fdfe37c94cad9
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-08-20 02:35:41 +0900

    tutorial of fan

--- src/stages/tutorial_stage.rs
+++ src/stages/tutorial_stage.rs
@@ -16,6 +16,63 @@ use crate::components::physics::force::Gravity;
 use crate::components::physics::material::PhysicMaterial;
 use crate::systems::field::{FIELD_HEIGHT, FIELD_WIDTH};
 
+pub fn fan_tutorial() -> StageInfo {
+    let block_list = vec![
+        BlockInfo {
+            pos: Vec2::new(0.0, -300.0),
+            block_shape_info: BlockShapeInfo::Rect {
+                extents: Vec2::new(200.0, 70.0),
+                rect_origin: Vec2::ZERO,
+                rotate_strategy: RotateStrategy::NoRotate,
+                slide_strategy: SlideStrategy::NoSlide,
+            },
+            block_attachment: vec![BlockAttachment::Fan(Fan::new(true, 0, 0.1))],
+            ..Default::default()
+        },
+        BlockInfo {
+            pos: Vec2::new(0.0, 250.0),
+            block_shape_info: BlockShapeInfo::Rect {
+                extents: Vec2::new(200.0, 150.0),
+                rect_origin: Vec2::ZERO,
+                rotate_strategy: RotateStrategy::NoRotate,
+                slide_strategy: SlideStrategy::NoSlide,
+            },
+            ..Default::default()
+        },
+    ];
+
+    let launcher_info = LauncherInfo {
+        pos: Vec2::new(-FIELD_WIDTH / 2.0 + 30.0, FIELD_HEIGHT / 2.0 - 30.0),
+        default_angle: 0.0,
+        rotate_speed: 0.02,
+        min_angle: -FRAC_PI_2,
+        max_angle: 0.0,
+    };
+
+    let mut ball_list = Vec::<BallInfo>::new();
+    ball_list.set_balls(BallType::Normal, 1);
+
+    let goal_list = vec![GoalInfo {
+        pos: Vec2::new(FIELD_WIDTH / 2.0 - 20.0, FIELD_HEIGHT / 2.0 - 20.0),
+        radius: 40.0,
+        score: 1,
+    }];
+
+    StageInfo {
+        stage_title: "tutorial[fan]",
+        time: 30 * 60,
+        launcher: launcher_info,
+        blocks: field_block()
+            .into_iter()
+            .chain(block_list)
+            .collect::<Vec<BlockInfo>>(),
+        balls: ball_list,
+        goal_pos: goal_list,
+        switches: vec![],
+        gravity: None,
+    }
+}
+
 pub fn tutorial_stage1() -> StageInfo {
     let material = PhysicMaterial::new(1.0, 1.0, 0.0);
     let block_list = vec![
