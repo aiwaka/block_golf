494e211fdddcbf01e3c1a54d40c020775ddb647d
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-08-19 18:07:51 +0900

    fix stages

--- src/stages/jamming.rs
+++ src/stages/jamming.rs
@@ -1,13 +1,13 @@
-use std::f32::consts::FRAC_PI_2;
+use std::f32::consts::FRAC_PI_4;
 
 use bevy::prelude::*;
+use itertools::Itertools;
 
 use super::field_blocks::field_block;
 use super::structs::{
     ArrangeBallInfo, BallInfo, BlockInfo, BlockShapeInfo, GoalInfo, LauncherInfo, StageInfo,
     SwitchInfo,
 };
-use crate::components::block::BlockSlidePath;
 use crate::components::block_attach::switch::{SwitchReceiver, SwitchType};
 use crate::components::block_attach::BlockAttachment;
 use crate::components::physics::material::PhysicMaterial;
@@ -15,37 +15,41 @@ use crate::components::{
     ball::BallType,
     block::{RotateStrategy, SlideStrategy},
 };
-use crate::systems::field::{FIELD_HEIGHT, FIELD_WIDTH};
+use crate::systems::field::FIELD_WIDTH;
 
 pub fn jamming1() -> StageInfo {
     let material = PhysicMaterial::new(1.0, 1.0, 0.0);
     let mut block_list = Vec::<BlockInfo>::new();
 
     // ブロックが退避する動き
-    const ESCAPE_MOVE_0: fn(i32) -> Vec2 = |count: i32| Vec2::new(0.0, -count as f32 * 4.0);
-    const ESCAPE_MOVE_1: fn(i32) -> Vec2 = |count: i32| Vec2::new(0.0, count as f32 * 4.0);
+    const ESCAPE_MOVE_0: fn(i32) -> Vec2 = |count: i32| Vec2::new(0.0, -count as f32 * 6.0);
+    const ESCAPE_MOVE_1: fn(i32) -> Vec2 = |count: i32| Vec2::new(0.0, count as f32 * 6.0);
 
-    for i in 0..2 {
-        for j in -3..3 {
-            let pos_x = i as f32 * 50.0;
-            let pos_y = j as f32 * 70.0 + 35.0;
+    for i in 0..4i32 {
+        for j in -2..2i32 {
+            let pos_x = (i / 2) as f32 * 220.0 + (i % 2) as f32 * 60.0 - 50.0;
+            let pos_y = j as f32 * 120.0 + 60.0;
             let block_attachment = vec![BlockAttachment::SwitchReceiver {
                 receiver: SwitchReceiver {
                     switch_type: SwitchType::MoveBlock {
-                        count: 0,
-                        limit: 60,
+                        range: (0..60).collect_vec(),
                         func: if j < 0 { ESCAPE_MOVE_0 } else { ESCAPE_MOVE_1 },
                     },
-                    target_id: 0,
+                    target_id: (i / 2) as u32,
                 },
             }];
+
+            // 回転の向きを決める
+            // iが奇数なら前半が1.0, 偶数なら前半が-1.0
+            // jが-2, -1なら後半が-1.0, 0, 1なら1.0になる.
+            let rotate_sgn = -1.0 * ((i % 2) * 2 - 1).signum() as f32 * (j as f32 + 0.5).signum();
 
             block_list.push(BlockInfo {
                 pos: Vec2::new(pos_x, pos_y),
                 block_shape_info: BlockShapeInfo::Rect {
-                    extents: Vec2::new(30.0, 40.0),
+                    extents: Vec2::new(30.0, 100.0),
                     rect_origin: Vec2::ZERO,
-                    rotate_strategy: RotateStrategy::Auto(0.1),
+                    rotate_strategy: RotateStrategy::Auto(0.08 * rotate_sgn),
                     slide_strategy: SlideStrategy::NoSlide,
                 },
                 material,
@@ -58,19 +62,26 @@ pub fn jamming1() -> StageInfo {
     let launcher_info = LauncherInfo {
         pos: Vec2::new(-FIELD_WIDTH / 2.0 + 30.0, 0.0),
         default_angle: 0.0,
-        rotate_speed: 0.0,
-        min_angle: 0.0,
-        max_angle: 0.0,
+        rotate_speed: 0.05,
+        min_angle: -FRAC_PI_4,
+        max_angle: FRAC_PI_4,
     };
 
     let mut ball_list = Vec::<BallInfo>::new();
-    ball_list.set_balls(BallType::Normal, 5);
+    ball_list.set_balls(BallType::Normal, 3);
 
-    let goal_list = vec![GoalInfo {
-        pos: Vec2::new(FIELD_WIDTH / 2.0 - 30.0, 0.0),
-        radius: 30.0,
-        score: 1,
-    }];
+    let goal_list = vec![
+        GoalInfo {
+            pos: Vec2::new(-120.0, 0.0),
+            radius: 30.0,
+            score: 1,
+        },
+        GoalInfo {
+            pos: Vec2::new(300.0, 0.0),
+            radius: 30.0,
+            score: 3,
+        },
+    ];
 
     let switches = vec![
         SwitchInfo {
@@ -81,8 +92,8 @@ pub fn jamming1() -> StageInfo {
         },
         SwitchInfo {
             target_id: 1,
-            auto_reverse: Some(180),
-            pos: Vec2::new(100.0, 0.0),
+            auto_reverse: Some(90),
+            pos: Vec2::new(-320.0, 0.0),
             ..Default::default()
         },
     ];
