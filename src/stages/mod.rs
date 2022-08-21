3e85d29abb0ee5917b3ec7ba25b04c7334f9a730
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-08-21 18:05:13 +0900

    add a tutorial stage of magnet

--- src/stages/mod.rs
+++ src/stages/mod.rs
@@ -3,7 +3,9 @@ use self::{
     jamming::jamming1,
     planet::{square_planet, strange_gravity},
     structs::StageInfo,
-    tutorial_stage::{fan_tutorial, gravity_tutorial, switch_tutorial, tutorial1, tutorial2},
+    tutorial_stage::{
+        fan_tutorial, gravity_tutorial, magnet_tutorial, switch_tutorial, tutorial1, tutorial2,
+    },
 };
 use itertools::Itertools;
 
@@ -24,6 +26,7 @@ fn stage_vec() -> Vec<GenerateStageInfoFunc> {
         tutorial1,
         tutorial2,
         fan_tutorial,
+        magnet_tutorial,
         switch_tutorial,
         gravity_tutorial,
         // sample_stage,
